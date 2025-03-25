ba7cf05afc26272a38c8eb619485e1e54273120a
bredontraining <mmanoj.cloudops@gmail.com>
2025-03-25 09:22:17 +0530

    Update Jenkins_declarativepipeline.groovy

--- Jenkins_declarativepipeline.groovy
+++ Jenkins_declarativepipeline.groovy
@@ -2,13 +2,19 @@ pipeline {
   agent any
   stages{
     stage('Git clone') {
+      steps{
       echo "Cloning the repo......"
+      }
     }
     stage('package'){
+      steps{
       echo "creating package ....."
+      }
     }
     stage('Deploy'){
+      steps{
       echo "Deploying......"
+      }
     }
   }
 }
